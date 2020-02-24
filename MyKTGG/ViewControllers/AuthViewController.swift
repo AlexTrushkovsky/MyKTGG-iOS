529ab56b4f62a2242d9c06730c07cf9e8726a952
AlexTrushkovsky <59797062+AlexTrushkovsky@users.noreply.github.com>
2020-02-24 16:20:52 +0200

    Update AuthViewController.swift

--- MyKTGG/ViewControllers/AuthViewController.swift
+++ MyKTGG/ViewControllers/AuthViewController.swift
@@ -152,7 +152,6 @@ extension AuthViewController: GIDSignInDelegate{
             }
             
             print("Successfully logged into Firebase with Google")
-            print("prirsgdzraer")
             self.dismiss(animated: true, completion: nil)
         }
     }
