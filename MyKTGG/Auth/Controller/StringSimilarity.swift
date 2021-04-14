7f58da5e12b1d816b69c6edcf871874fe326e36f
Alex <trushkovskya@gmail.com>
2021-04-14 18:54:51 +0300

    added group getting from teams; fixed bug when group choose vc presents without group data

---
+++ MyKTGG/Auth/Controller/StringSimilarity.swift
@@ -0,0 +1,9 @@
+//
+//  StringSimilarity.swift
+//  MyKTGG
+//
+//  Created by Алексей Трушковский on 09.04.2021.
+//  Copyright © 2021 Алексей Трушковский. All rights reserved.
+//
+
+import Foundation
